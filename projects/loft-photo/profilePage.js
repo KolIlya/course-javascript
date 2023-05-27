3cf24c392f2a817420600a2c5a727465423d2407
Ilya Kolpakov <ilya.kolpakofffgmail.com>
2023-05-27 21:54:32 +0300

    Done loft-photo week6 homework

--- projects/loft-photo/profilePage.js
+++ projects/loft-photo/profilePage.js
@@ -16,40 +16,39 @@ export default {
     photosComponent.innerHTML = '';
 
     for (const photo of photos.items) {
-        const size = model.findSize(photo);
-        const element = document.createElement('div');
+      const size = model.findSize(photo);
+      const element = document.createElement('div');
 
-        element.classList.add('component-user-photo');
-        element.dataset.id = photo.id;
-        element.style.backgroundImage = `url('${size.url}')`;
-        photosComponent.append(element);
-
-        // element.dataset.url = photo.url;
+      element.classList.add('component-user-photo');
+      element.dataset.id = photo.id;
+      element.style.backgroundImage = `url('${size.url}')`;
+      photosComponent.append(element);
     }
   },
 
   handleEvents() {
-    document.querySelector('component-user-photos').addEventListener('click', async (e) => {
+    document
+      .querySelector('component-user-photos')
+      .addEventListener('click', async (e) => {
         if (e.target.classList.contains('component-user-photo')) {
-            const photoId = e.target.dataset.id;
-            // const photoUrl = ;
+          const photoId = e.target.dataset.id;
 
-            const size = model.findSize(photo);
-            const friendPhotos = await model.getPhotos(this.user.id);
-            const photo = friendPhotos.items.find((photo) => photo.id == photoId);
+          const size = model.findSize(photo);
+          const friendPhotos = await model.getPhotos(this.user.id);
+          const photo = friendPhotos.items.find((photo) => photo.id == photoId);
 
-            mainPage.setFriendAndPhoto(this.user, parseInt(photoId), size.url);
-            pages.openPage('main');
-        }   
-    });
+          mainPage.setFriendAndPhoto(this.user, parseInt(photoId), size.url);
+          pages.openPage('main');
+        }
+      });
 
     document.querySelector('.page-profile-back').addEventListener('click', async () => {
-        pages.openPage('main');
+      pages.openPage('main');
     });
 
     document.querySelector('.page-profile-exit').addEventListener('click', async () => {
-        await model.logout();
-        pages.openPage('login');
+      await model.logout();
+      pages.openPage('login');
     });
   },
 };
